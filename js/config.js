6021998ac8dbb72c96d57f4c02a6f2ffe25b2756
JuanPerez115 <60701231+JuanPerez115@users.noreply.github.com>
2021-01-06 13:52:13 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,29 @@
+/**
+ * Color de fondo para el canvas.
+ * @type {String}
+ */
+const COLOR_FONDO = "#e6f7ff";
+
+/**
+ * Color de los puntos que representan las ciudades en el canvas.
+ * @type {String}
+ */
+const COLOR_PUNTOS = "#000000";
+
+/**
+ * Color de las lineas que representan una ruta en el canvas.
+ * @type {String}
+ */
+const COLOR_LINEAS = "#8b8b8b";
+
+/**
+ * Imagen que representa el inicio de una ruta en el canvas.
+ * @type {String}
+ */
+const PATH_IMG_START = "img/start2.png";
+
+/**
+ * Imagen que representa el final de una ruta en el canvas.
+ * @type {String}
+ */
+const PATH_IMG_FINISH = "img/finish.png";
